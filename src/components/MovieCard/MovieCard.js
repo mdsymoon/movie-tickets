f5438f8b799039facbe9127f34529fec55de6db1
mdsymoon <mdsymoon11@gmail.com>
2021-11-27 18:23:21 -0800

    folder setup done

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,11 @@
+import "./MovieCard.css";
+import React from 'react';
+const MovieCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MovieCard;
---
+++ src/pages/MovieDetails/MovieDetails.js
@@ -0,0 +1,12 @@
+import "./MovieDetails.css";
+import React from 'react';
+
+const MovieDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MovieDetails;
