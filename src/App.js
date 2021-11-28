a56947e52c72c00c71b7ae3351cbfbcd514a8210
mdsymoon <mdsymoon11@gmail.com>
2021-11-27 19:06:57 -0800

    fetching movie data in home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+import { createContext, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Bookings from "./pages/Bookings/Bookings";
 
-function App() {
-  return (
-    <Router>
-      <Header />
+export const MovieList = createContext();
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bookings" element={<Bookings />} />
-      </Routes>
-    </Router>
+function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  return (
+    <MovieList.Provider value={[movieList, setMovieList]}>
+      <Router>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bookings" element={<Bookings />} />
+        </Routes>
+      </Router>
+    </MovieList.Provider>
   );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,28 @@
 import "./Home.css";
-import React from 'react';
+import React, { useContext, useEffect } from "react";
+import { MovieList } from "../../App";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [movieList, setMovieList] = useContext(MovieList);
+
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/search/shows?q=all")
+      .then((res) => res.json())
+      .then((data) => setMovieList(data));
+  });
+
+  return (
+    <Container>
+      {
+          movieList.map((movie) =>
+          <div>
+              {movie.show.name}
+          </div>
+          )
+      }
+    </Container>
+  );
 };
 
 export default Home;
